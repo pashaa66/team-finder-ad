5d0dbb4cd543416966a5f213f4966312e36a3e28
Pavel Petrov <pasha10428@gmail.com>
2026-05-22 20:18:06 +0200

    add views for project detail, participate, complete

--- projects/urls.py
+++ projects/urls.py
@@ -6,4 +6,13 @@ app_name = 'projects'
 
 urlpatterns = [
     path('list/', views.project_list, name='list'),
+    path('<int:project_id>/', views.project_detail, name='detail'),
+    path(
+        '<int:project_id>/complete/', views.complete_project, name='complete'
+    ),
+    path(
+        '<int:project_id>/toggle-participate/',
+        views.toggle_participate,
+        name='toggle_participate',
+    ),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, render
+from django.views.decorators.http import require_POST
 
 from .models import Project
 
@@ -11,3 +14,50 @@ def project_list(request):
     return render(
         request, 'projects/project_list.html', {'projects': page_obj}
     )
+
+
+def project_detail(request, project_id):
+    project = get_object_or_404(Project, pk=project_id)
+    return render(
+        request, 'projects/project-details.html', {'project': project}
+    )
+
+
+@login_required
+@require_POST
+def toggle_participate(request, project_id):
+    project = get_object_or_404(Project, pk=project_id)
+    user = request.user
+
+    if user == project.owner:
+        return JsonResponse(
+            {'status': 'error', 'message': 'Owner cannot leave own project'},
+            status=400,
+        )
+
+    if user in project.participants.all():
+        project.participants.remove(user)
+    else:
+        project.participants.add(user)
+
+    return JsonResponse({'status': 'ok'})
+
+
+@login_required
+@require_POST
+def complete_project(request, project_id):
+    project = get_object_or_404(Project, pk=project_id)
+
+    if project.owner != request.user or project.status != 'open':
+        return JsonResponse(
+            {
+                'status': 'error',
+                'message': 'Forbidden or project already closed',
+            },
+            status=400,
+        )
+
+    project.status = 'closed'
+    project.save()
+
+    return JsonResponse({'status': 'ok', 'project_status': 'closed'})
